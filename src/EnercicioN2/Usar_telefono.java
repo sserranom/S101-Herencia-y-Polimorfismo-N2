44d9e1aaa578217267bc7ba9e2eaf2153198ae5f
sserranom <sergiomelgara@gmail.com>
2025-01-29 00:12:31 +0100

    Agregamos dos lineas de código para mostrar la marca y el modelo del telefono en la clase Usar_telefono

--- src/EnercicioN2/Usar_telefono.java
+++ src/EnercicioN2/Usar_telefono.java
@@ -5,12 +5,22 @@ public class Usar_telefono {
 	public static void main(String[] args) {
 		
 		Smartphone iphone = new Smartphone("Iphone", "6s");
+		Smartphone samsung = new Smartphone("Galaxi", "S21");
+		
+		System.out.println("Telefono " + iphone.marca + " " + iphone.modelo);
 		
 		iphone.llamar("664446610");
 		
 		iphone.fotografiar();
 		
 		iphone.alarma();
+		
+		System.out.println("\n");
+		
+		System.out.println("Telefono " + samsung.marca + " " + samsung.modelo);
+		samsung.llamar("77777968");
+		samsung.fotografiar();
+		samsung.alarma();
 		
 
 	}
